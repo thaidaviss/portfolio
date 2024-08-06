930c3181ae3fc6674c4711dd714ccc329ee77989
Thai Duc <thaibui1310@gmail.com>
2024-08-06 22:13:42 +0700

    update resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,29 @@
+'use client';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import React from 'react';
-
-function Resume() {
-  return <div>Resume</div>;
+import { motion } from 'framer-motion';
+function Resume () {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 1, duration: 0.4, ease: 'easeIn' } }}
+      className='container mx-auto h-full py-12'>
+      <div className='mx-auto'>
+        <Tabs defaultValue='education' className='flex flex-col md:flex-row gap-10'>
+          <TabsList className='flex flex-col h-full gap-5 bg-transparent'>
+            <TabsTrigger value='experience'>Experience</TabsTrigger>
+            <TabsTrigger value='education'>Education</TabsTrigger>
+            <TabsTrigger value='about-me'>About Me</TabsTrigger>
+            <TabsTrigger value='skill'>Skill</TabsTrigger>
+          </TabsList>
+          <TabsContent value='experience'>Experience</TabsContent>
+          <TabsContent value='education'>Education</TabsContent>
+          <TabsContent value='about-me'>About Me</TabsContent>
+          <TabsContent value='skill'>Skill</TabsContent>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 }
 
 export default Resume;
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,44 @@
-
+'use client';
+const services = [
+  {
+    number: '01',
+    name: '',
+    description: '',
+  },
+  {
+    number: '02',
+    name: '',
+    description: '',
+  },
+  {
+    number: '03',
+    name: '',
+    description: '',
+  },
+  {
+    number: '04',
+    name: '',
+    description: '',
+  },
+  {
+    number: '05',
+    name: '',
+    description: '',
+  },
+  {
+    number: '06',
+    name: '',
+    description: '',
+  },
+];
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div>
+      {services.map(item => (
+        <div key={item.number}>{item.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default Services;
